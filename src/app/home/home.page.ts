127ebbae4f15e0bd17fdc1592a9551243b0bdd56
Luiz Amorim <135264514+JZLZ@users.noreply.github.com>
2025-06-20 15:31:35 -0300

    List + StyleCSS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public pokemons: any[] = [];
 
-  constructor() {}
-
+  constructor(private httpService: HttpService) {}
+  ngOnInit() {
+    this.getPokemons();
+  } 
+  getPokemons() {
+    this.httpService.getPokemons().subscribe((data: any) => this.pokemons = data.results);
+  }
+  
 }
