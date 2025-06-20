938ffeafb00aa5bbdb3e20d5f9d55b460d3d6c4d
Luiz Amorim <135264514+JZLZ@users.noreply.github.com>
2025-06-20 13:50:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pokepad',
+  webDir: 'www'
+};
+
+export default config;
